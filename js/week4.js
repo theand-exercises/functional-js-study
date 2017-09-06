e32fb1929dadf47e98849b7b725e71b6308f5bf7
Heesang Chae <theand@gmail.com>
2017-09-06 20:03:13 +0900

    week4 - 필터링  체크 하다가 만 상태.

--- js/week4.js
+++ js/week4.js
@@ -1,5 +1,7 @@
-!function(lo) {
-    function to_number(str) { return parseInt(str.replace(',', '')) }
+!function (lo) {
+    function to_number(str) {
+        return parseInt(str.replace(',', ''))
+    }
 
     _.each($('.movie_box'), __(
         _.c(movies),
@@ -12,17 +14,17 @@
             label 등급 
             .inputs {{_.go($, _.map(m => m.movie_rating), _.uniq, _.sum(`, _.t$(`
               input[type=checkbox name=movie_rating value='{{$}}'] {{$}}
-            `) ,`))}}
+            `), `))}}
           .genre
             label 장르 
             .inputs {{_.go($, _.map(m => m.genre), _.uniq, _.sum(`, _.t$(`
               input[type=checkbox name=genre value='{{$}}'] {{$}}
-            `) ,`))}}
+            `), `))}}
           .director
             label 감독 
             .inputs {{_.go($, _.map(m => m.director), _.uniq, _.sum(`, _.t$(`
               input[type=checkbox name=director value='{{$}}'] {{$}}
-            `) ,`))}}
+            `), `))}}
         .sort
           label 정렬
           select
@@ -33,7 +35,7 @@
       .body
         ul.movie_list {{_.go($, `, lo.items = _.sum(_.t$(`
           li.movie_item {{$.name}} | {{$.date}} | {{$.director}} | {{$.genre}} | {{$.movie_rating}} [ {{$.attendance}} | {{$.like}} | {{$.comment}} ]
-        `)) ,`)}}
+        `)), `)}}
         .extension
           .btns
             button.btn1 가장 개봉한 영화가 가장 많았던 해의 총 관람객 수
@@ -46,62 +48,65 @@
 
 
         _.c('.movie_box'), $,
-        $.on('change', '.filter input[type=checkbox]', function() {
-            // console.log(this.name);
-            // console.log(this.value);
+        $.on('change', '.filter input[type=checkbox]',
+            _.pipe(
+                function () {
+                    // console.log(this.name);
+                    // console.log(this.value);
 
-            //only rating
-            console.log("===========only rating");
-            const checked_value = _.map(document.querySelectorAll("input[name=movie_rating]:checked"), e => e.value);
-            console.log("checked_value : ", checked_value);
-
-
-            console.log("matching : ", _filter(movies, e=> _.contains(checked_value, e.movie_rating) ));
-
+                    //only rating
+                    console.log("===========only rating");
+                    const checked_value = _.map(document.querySelectorAll("input[name=movie_rating]:checked"), e => e.value);
+                    console.log("checked_value : ", checked_value);
 
 
-            //whole checked
-            console.log("===========complex");
-            const whole_checked_value_map = _.reduce( document.querySelectorAll("input:checked"), (result, c) => {
-                if( !result[c.name]){
-                    result[c.name] = [];
-                }
-                result[c.name].push(c.value);
-                result[c.name] = _.uniq(result[c.name]);
-                return result;
+                    const only_rating = _filter(movies, e => _.contains(checked_value, e.movie_rating));
+                    console.log("matching : ", only_rating);
 
-            }, {});
-            console.log("whole_checked_value_map : ", whole_checked_value_map);
 
-            // function matchingCriteria(value_map, e){
-            //     "use strict";
-            //     _.reduce(value_map, (result, o)=>{
-            //         cosole.log(o);
-            //         cosole.log(e);
-            //         if( result )
-            //             return result;
-            //         return _.contains(o, e)
-            //
-            //     }, false);
-            // }
+                    //whole checked
+                    console.log("===========complex");
+                    const whole_checked_value_map = _.reduce(document.querySelectorAll("input:checked"), (result, c) => {
+                        if (!result[c.name]) {
+                            result[c.name] = [];
+                        }
+                        result[c.name].push(c.value);
+                        result[c.name] = _.uniq(result[c.name]);
+                        return result;
 
-            // console.log( _filter(movies, e=> matchingCriteria(whole_checked_value_map, e) ));
+                    }, {});
+                    console.log("whole_checked_value_map : ", whole_checked_value_map);
+
+                    // function matchingCriteria(value_map, m){
+                    //     return _.reduce(value_map, (result, o)=>{
+                    //         cosole.log(o);
+                    //         cosole.log(m);
+                    //         if( result )
+                    //             return result;
+                    //         return _.contains(o, m)
+                    //
+                    //     }, false);
+                    // }
+                    //
+                    // console.log( _filter(movies, m=> matchingCriteria(whole_checked_value_map, m) ));
+
+                    return only_rating;
+                },
+                lo.items,
+                $.html_to('movie_list')
+            )),
+
+        $.on('change', '.sort select', function () {
 
         }),
 
-        $.on('change', '.sort select', function() {
+        $.on('click', '.extension .btn1', function () {
 
         }),
 
-        $.on('click', '.extension .btn1', function() {
-
-        }),
-
-        $.on('click', '.extension .btn2', function() {
+        $.on('click', '.extension .btn2', function () {
 
         })
-
-
     ))
 
 }({});
